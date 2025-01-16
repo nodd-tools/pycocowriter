c264fd9dcf9de3549131f520b9d4bc6410ed3060
cscott brown <scott.brown@noaa.gov>
2025-01-16 11:36:05 -0600

    fixing config bug failing when no keypoints config present

--- tests/test_csv2coco.py
+++ tests/test_csv2coco.py
@@ -112,6 +112,91 @@ class TestCSV2COCO(unittest.TestCase):
         self.assertDictEqual(coco_dict, TestCSV2COCO.example_csv_as_coco)
 
 
+class TestCSV2COCONoKeypoints(unittest.TestCase):
+
+    example_config = {
+        'meta': {
+            'skiprows': 2
+        },
+        'filename': 3,
+        'label': 10,
+        'bbox_tlbr': {
+            'tlx': 4,
+            'tly': 5,
+            'brx': 6,
+            'bry': 7
+        }
+    }
+
+    example_csv = [
+        ['err'] * 17,
+        ['err'] * 17,
+        ['err', 'err', 'err', 'filename1', 10, 11, 20, 21, 'err', 'err', 'label1', 'err', 'err', 1, 2, 3, 4, 'err'],
+        ['err', 'err', 'err', 'filename2', 100, 110, 200, 210, 'err', 'err', 'label1', 'err', 'err', 10, 20, 70, 80, 'err'],
+        ['err', 'err', 'err', 'filename1', 1000, 1100, 2000, 2100, 'err', 'err', 'label2', 'err', 'err', 100, 200, 300, 400, 'err'],
+    ]
+
+    example_csv_as_coco = {
+        'info': {},
+        'images': [
+            {'id': 0, 'file_name': 'filename1'},
+            {'id': 1, 'file_name': 'filename2'}
+        ],
+        'annotations': [
+            {
+                'image_id': 0,
+                'id': 0,
+                'category_id': 0,
+                'bbox': (10, 11, 10, 10),
+                'area': 100,
+                'iscrowd': 0
+            },
+            {
+                'image_id': 1,
+                'id': 1,
+                'category_id': 0,
+                'bbox': (100, 110, 100, 100),
+                'area': 10000,
+                'iscrowd': 0
+            },
+            {
+                'image_id': 0,
+                'id': 2,
+                'category_id': 1,
+                'bbox': (1000, 1100, 1000, 1000),
+                'area': 1000000,
+                'iscrowd': 0
+            }
+        ],
+        'licenses': [],
+        'categories': [
+            {
+                'name': 'label1',
+                'id': 0
+            },
+            {
+                'name': 'label2',
+                'id': 1
+            }
+        ]
+    }
+
+    def test_csv_parser(self):
+        csv2coco_parser = pycocowriter.csv2coco.Iterable2COCO(
+            pycocowriter.csv2coco.Iterable2COCOConfig(TestCSV2COCONoKeypoints.example_config)
+        )
+        images, annotations, categories = csv2coco_parser.parse(iter(TestCSV2COCONoKeypoints.example_csv))
+        coco_data = COCOData(
+            COCOInfo(),
+            images,
+            annotations,
+            [],
+            categories
+        )
+        coco_dict = coco_data.to_dict()
+        self.assertDictEqual(coco_dict, TestCSV2COCONoKeypoints.example_csv_as_coco)
+
+
 def load_tests(loader, tests, ignore):
     return utils.doctests(pycocowriter.csv2coco, tests)
 
